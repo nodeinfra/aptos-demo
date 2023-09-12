35ed07604ed22bca8b22bcb58dc7036adb25184d
Desmos <desmos@Desmosui-MacBookPro.local>
2023-09-12 18:01:35 +0900

    Fix something

--- demo.py
+++ demo.py
@@ -17,6 +17,13 @@ addresses = {
     'msafe': '0xaa90e0d9d16b63ba4a289fb0dc8d1b454058b21c9b5c76864f825d5c1f32582e',
     'topaz': '0x2c7bccf7b31baf770fdbcc768d9e9cb3d87805e255355df5db32ac9a669010a2'
 }
+period = {
+    '1h': 1,
+    '1D': 24,
+    '3D': 72,
+    '7D': 168,
+    '30D': 720
+}
 
 if 'selected_table' not in st.session_state:
     st.session_state.selected_table = 'Overview'
@@ -67,23 +74,79 @@ elif st.session_state.selected_table == 'thala' :
     st.session_state.selected_event = st.sidebar.selectbox('Choose a type:', event_types)
     
     if st.session_state.selected_event == 'Volume' :
-        current_time = datetime.now(utc_timezone)
-        time_24_hours_ago = current_time - timedelta(hours=24)
-        formatted_time = time_24_hours_ago.strftime("%Y-%m-%d %H:%M:%S.%f")
+        st.session_state.selected_period = st.sidebar.selectbox('Choose a period:', ["1h", "1D", "3D", "7D", "30D"])
+        if st.session_state.selected_period != None :
+            current_time = datetime.now(utc_timezone)
+            time_ago = current_time - timedelta(hours=period[st.session_state.selected_period])
+            formatted_time = time_ago.strftime("%Y-%m-%d %H:%M:%S.%f")
 
-        query = get_volume_data_query(formatted_time)
-        data = fetch_graphql_data(query, "https://aptos-mainnet.nodeinfra.com/indexer")
+            query = get_volume_data_query(formatted_time)
+            data = fetch_graphql_data(query, "https://aptos-mainnet.nodeinfra.com/indexer")
 
-        events_data = [event["data"] for event in data["events"]]
+            events_data = [event["data"] for event in data["events"]]
 
-        df = pd.DataFrame(events_data)
-        df['amount'] = df['amount'].astype(int)
-        total_amount = df['amount'].sum()
+            df = pd.DataFrame(events_data)
+            df['amount'] = df['amount'].astype(int)
+            total_amount = df['amount'].sum()
 
-        st.write("### GraphQL Query:")
-        st.code(query)
-        st.write("### Trading Volume (24h)")
-        st.write("###", total_amount)
+            divided_value = total_amount / 10**8
+            formatted_value = "{:,.2f}".format(divided_value)
+
+            st.write(f"### Trading Volume ({st.session_state.selected_period})")
+            st.write("### GraphQL Query:")
+            st.code(query)
+            st.write("###", formatted_value, "APT")
+
+        # time_3_days_ago = current_time - timedelta(hours=72)
+        # formatted_time = time_3_days_ago.strftime("%Y-%m-%d %H:%M:%S.%f")
+
+        # query = get_volume_data_query(formatted_time)
+        # data = fetch_graphql_data(query, "https://aptos-mainnet.nodeinfra.com/indexer")
+
+        # events_data = [event["data"] for event in data["events"]]
+
+        # df = pd.DataFrame(events_data)
+        # df['amount'] = df['amount'].astype(int)
+        # total_amount = df['amount'].sum()
+
+        # st.write("### Trading Volume (3D)")
+        # st.write("### GraphQL Query:")
+        # st.code(query)
+        # st.write("###", total_amount)
+
+        # time_7_days_ago = current_time - timedelta(hours=168)
+        # formatted_time = time_7_days_ago.strftime("%Y-%m-%d %H:%M:%S.%f")
+
+        # query = get_volume_data_query(formatted_time)
+        # data = fetch_graphql_data(query, "https://aptos-mainnet.nodeinfra.com/indexer")
+
+        # events_data = [event["data"] for event in data["events"]]
+
+        # df = pd.DataFrame(events_data)
+        # df['amount'] = df['amount'].astype(int)
+        # total_amount = df['amount'].sum()
+
+        # st.write("### Trading Volume (7D)")
+        # st.write("### GraphQL Query:")
+        # st.code(query)
+        # st.write("###", total_amount)
+
+        # time_30_days_ago = current_time - timedelta(hours=720)
+        # formatted_time = time_30_days_ago.strftime("%Y-%m-%d %H:%M:%S.%f")
+
+        # query = get_volume_data_query(formatted_time)
+        # data = fetch_graphql_data(query, "https://aptos-mainnet.nodeinfra.com/indexer")
+
+        # events_data = [event["data"] for event in data["events"]]
+
+        # df = pd.DataFrame(events_data)
+        # df['amount'] = df['amount'].astype(int)
+        # total_amount = df['amount'].sum()
+
+        # st.write("### Trading Volume (30D)")
+        # st.write("### GraphQL Query:")
+        # st.code(query)
+        # st.write("###", total_amount)
 
 
     elif st.session_state.selected_event != None :
@@ -111,6 +174,10 @@ elif st.session_state.selected_table != None :
     first_key = list(data.keys())[0]
 
     event_types = [event['type'] for event in data[first_key]]
+
+    if '0xaa90e0d9d16b63ba4a289fb0dc8d1b454058b21c9b5c76864f825d5c1f32582e::creator::MomentumSafeCreation' in event_types :
+        event_types.remove('0xaa90e0d9d16b63ba4a289fb0dc8d1b454058b21c9b5c76864f825d5c1f32582e::creator::MomentumSafeCreation')
+        event_types.insert(0, '0xaa90e0d9d16b63ba4a289fb0dc8d1b454058b21c9b5c76864f825d5c1f32582e::creator::MomentumSafeCreation')
 
     st.session_state.selected_event = st.sidebar.selectbox('Choose a type:', event_types)
     
